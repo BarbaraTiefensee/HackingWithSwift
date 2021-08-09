227bca4ebc2db970e36d3518922b00e7c7a99ca8
BarbaraTiefensee <60518782+BarbaraTiefensee@users.noreply.github.com>
2021-08-09 14:08:27 -0300

    Add files via upload

---
+++ HackingWithSwift_02/HackingWithSwiff_02/Extension/UIImage+Extension.swift
@@ -0,0 +1,60 @@
+//
+//  UIImage+Extension.swift
+//  HackingWithSwiff_02
+//
+//  Created by Bárbara Tiefensee on 06/08/21.
+//
+import UIKit
+import Foundation
+
+extension UIImage {
+    
+    open class var estonia: UIImage? {
+        return UIImage(named: "estonia")
+    }
+    
+    open class var france: UIImage? {
+        return UIImage(named: "france")
+    }
+    
+    open class var germany: UIImage? {
+        return UIImage(named: "germany")
+    }
+    
+    open class var irleand: UIImage? {
+        return UIImage(named: "ireland")
+    }
+    
+    open class var italy: UIImage? {
+        return UIImage(named: "italy")
+    }
+    
+    open class var monaco: UIImage? {
+        return UIImage(named: "monaco")
+    }
+    
+    open class var nigeria: UIImage? {
+        return UIImage(named: "nigeria")
+    }
+    
+    open class var poland: UIImage? {
+        return UIImage(named: "poland")
+    }
+    
+    open class var russia: UIImage? {
+        return UIImage(named: "russia")
+    }
+    
+    open class var spain: UIImage? {
+        return UIImage(named: "spain")
+    }
+    
+    open class var uk: UIImage? {
+        return UIImage(named: "uk")
+    }
+    
+    open class var us: UIImage? {
+        return UIImage(named: "us")
+    }
+}
+
